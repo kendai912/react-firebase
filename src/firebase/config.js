f160657f7f516954e5a7172234135356cf99f50e
kendai912 <56577984+kendai912@users.noreply.github.com>
2021-04-13 20:48:28 +0900

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCXyTmTZmzTYwxp_rQoPLUbEoWnVTEjugM",
-  authDomain: "react-firebase-d79a3.firebaseapp.com",
-  projectId: "react-firebase-d79a3",
-  storageBucket: "react-firebase-d79a3.appspot.com",
-  messagingSenderId: "1032729960565",
-  appId: "1:1032729960565:web:581beb744e9350e66c983c",
-};
